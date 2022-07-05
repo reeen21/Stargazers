1d004131aacfd0d40bc44f3007a227b415387bc9
Ren <ren.t21@icloud.com>
2022-07-05 20:54:03 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'constellation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for constellation
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+end
--- constellation/ViewController.swift
+++ constellation/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var lat = 0.0
+    var lon = 0.0
+    var MonthAndDay = ""
+    var hour = ""
+    var min = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        date()
     }
-
-
+    func date() {
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd  HH mm"
+        let dateStr = formatter.string(from: now as Date)
+        let dataBase = dateStr.components(separatedBy: " ")
+        MonthAndDay = dataBase[0]
+        hour = dataBase[1]
+        min = dataBase[2]
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        lat = Double(location.coordinate.latitude)
+        lon = Double(location.coordinate.longitude)
+        let url = "https://livlog.xyz/hoshimiru/constellation?lat=\(lat)&lng=\(lon)&date=\(MonthAndDay)&hour=\(hour)&min=\(min)"
+       
+    }
+    
 }
+
+
+
 
